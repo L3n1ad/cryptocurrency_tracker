2d224982e0c227ce3a2bdb838a61bb7e0ed767db
L3n1ad <danieltoth0910@gmail.com>
2019-11-29 20:14:50 +0000

    rounded up value for currencies

--- src/components/currencyDetail.vue
+++ src/components/currencyDetail.vue
@@ -3,7 +3,7 @@
     <h3>Name: {{currency.name}}</h3>
     <img :src="currency.image" style="max-width: 70px">
     <p>Cyrrency symbol: {{currency.symbol}}</p>
-    <p>Currency price: GBP {{currency.current_price}}</p>
+    <p>Currency price: GBP {{currency.current_price.toFixed(2)}}</p>
     <p>Price change in the last 24 hours: {{currency.price_change_percentage_24h_in_currency.toFixed(2)}}</p>
     <p>Price change in the last 7 days: {{currency.price_change_percentage_7d_in_currency.toFixed(2)}}</p>
     <p>Price change in the last 30 days: {{currency.price_change_percentage_30d_in_currency.toFixed(2)}}</p>
