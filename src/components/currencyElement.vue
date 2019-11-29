84f80fbe999c3ff653676438660be480908471a8
L3n1ad <danieltoth0910@gmail.com>
2019-11-29 15:09:35 +0000

    created currencyElement.vue file and added options to dropdown

---
+++ src/components/currencyElement.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <option :value="currency">{{currency.name}}</option>
+</template>
+
+<script>
+export default {
+  name: 'currency-element',
+  props: ['currency'],
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
