4766a71c5e323c7df7f06bd08a577dbbedf71944
L3n1ad <danieltoth0910@gmail.com>
2019-11-29 15:09:14 +0000

    created currencyList.vue file and added dropdown

---
+++ src/components/currencyList.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <select v-model="selectedCurrency">
+    <currency-element v-for='(currency, index) in currencies' :kex='index' :currency='currency'></currency-element>
+  </select>
+</template>
+
+<script>
+import CurrencyElement from './currencyElement.vue'
+
+export default {
+  name: 'currencies-list',
+  props: ['currencies'],
+  data(){
+    return{
+      selectedCurrency: null
+    }
+  },
+  components: {
+    'currency-element': CurrencyElement
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
