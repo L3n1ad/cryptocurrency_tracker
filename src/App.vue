abe4aaf9ad5a0c77f250959b7b7def6cf8474f26
L3n1ad <danieltoth0910@gmail.com>
2019-11-29 15:21:44 +0000

    sorted data alphabetically by currency name;

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,12 @@ export default {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d')
   .then(response => response.json())
   .then(data => this.currencies = data)
+  .then(() => this.sortDataByName())
+},
+methods:{
+  sortDataByName(){
+    this.currencies = this.currencies.sort((dataA, dataB) => (dataA.name > dataB.name) ? 1 : (dataB.name > dataA.name) ? -1: 0)
+  }
 },
 components: {
   'currencies-list': CurrenciesList
