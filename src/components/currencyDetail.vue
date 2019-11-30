29ba481c6a734278fbf8c0cfd8826545a04bbb71
L3n1ad <danieltoth0910@gmail.com>
2019-11-30 13:24:45 +0000

    currencyDetail refactored to fit in the table html

--- src/components/currencyDetail.vue
+++ src/components/currencyDetail.vue
@@ -1,14 +1,15 @@
 <template lang="html">
-  <div class="currency-detail">
-    <h3>Name: {{currency.name}}</h3>
-    <img :src="currency.image" style="max-width: 70px">
-    <p>Cyrrency symbol: {{currency.symbol}}</p>
-    <p>Currency price: GBP {{currency.current_price.toFixed(2)}}</p>
-    <p>Price change in the last 24 hours: {{currency.price_change_percentage_24h_in_currency.toFixed(2)}}</p>
-    <p>Price change in the last 7 days: {{currency.price_change_percentage_7d_in_currency.toFixed(2)}}</p>
-    <p>Price change in the last 30 days: {{currency.price_change_percentage_30d_in_currency.toFixed(2)}}</p>
+  <tr>
+    <td><img :src="currency.image" style="max-width: 70px"></td>
+    <td>{{currency.name}}</td>
+    <td>{{currency.symbol}}</td>
+    <td>{{currency.current_price.toFixed(2)}}</td>
+    <td>{{currency.price_change_percentage_24h_in_currency.toFixed(2)}}</td>
+    <td>{{currency.price_change_percentage_7d_in_currency.toFixed(2)}}</td>
+    <td>{{currency.price_change_percentage_30d_in_currency.toFixed(2)}}</td>
     <currency-diagram :currency='currency'></currency-diagram>
-  </div>
+
+  </tr>
 </template>
 
 <script>
