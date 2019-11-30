9492eb30b5f7661ca0823d902d70cbab4f06ad9f
L3n1ad <danieltoth0910@gmail.com>
2019-11-30 13:25:51 +0000

    app vue refactored logic to show data moved to tableCurrency

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@
 
     <search-form :currencies="currencies"></search-form>
 
-    <currency-detail v-if="selectedCurrency" :currency="selectedCurrency"></currency-detail>
+    <!-- <currency-detail v-if="selectedCurrency" :currency="selectedCurrency"></currency-detail>
 
-    <currency-detail v-if="filteredCurrency.length !== 0> 0" v-for="currency in filteredCurrency" :currency="currency"></currency-detail>
+    if I change the order of the if and else it will slow the app down drastically and it will probably break it WHY?
 
-    <currency-detail v-for="currency in currencies" :currency="currency"></currency-detail>
+    <currency-detail v-else v-for="currency in filteredCurrency" :currency="currency"></currency-detail>
+
+    <currency-detail v-if="noFilteredCurrency" v-for="currency in currencies" :currency="currency"></currency-detail> -->
+
+    <currency-table :currencies='currencies' :filteredCurrency='filteredCurrency' :selectedCurrency='selectedCurrency' :noFilteredCurrency='noFilteredCurrency'></currency-table>
 
   </div>
 </template>
@@ -18,6 +22,7 @@
 import CurrencyDetail from './components/currencyDetail.vue'
 import {eventBus} from './main.js';
 import SearchForm from './components/searchForm.vue'
+import CurrencyTable from './components/tableCurrency.vue'
 
 export default {
   name: 'app',
@@ -32,11 +37,16 @@ export default {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d')
     .then(response => response.json())
     .then(data => this.currencies = data)
-    .then(() => this.sortDataByName())
+    .then(() => this.sortDataByName());
 
     eventBus.$on('selected-currency', (currency) => {this.selectedCurrency = currency;})
 
     eventBus.$on('filtered-currency', currencies => this.filteredCurrency = currencies)
+  },
+  computed: {
+    noFilteredCurrency(){
+      return this.filteredCurrency.length === 0
+    }
   },
   methods:{
     sortDataByName(){
@@ -45,7 +55,8 @@ export default {
   },
   components: {
     'currency-detail': CurrencyDetail,
-    'search-form': SearchForm
+    'search-form': SearchForm,
+    'currency-table': CurrencyTable
   }
 }
 </script>
