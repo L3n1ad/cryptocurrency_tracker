e270c23ce60a0b507d4cbfd702f595252f6a5b60
L3n1ad <danieltoth0910@gmail.com>
2019-11-30 13:24:20 +0000

    table component created

---
+++ src/components/tableCurrency.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <table>
+    <thead>
+      <tr>
+        <th>Icon</th>
+        <th>Name</th>
+        <th>Symbol</th>
+        <th>Price</th>
+        <th>24h</th>
+        <th>7d</th>
+        <th>30d</th>
+        <th>Diagram</th>
+      </tr>
+    </thead>
+    <tbody>
+      <currency-detail v-if="selectedCurrency" :currency="selectedCurrency"></currency-detail>
+
+      <!-- if I change the order of the if and else it will slow the app down drastically and it will probably break it WHY? -->
+
+      <currency-detail v-else v-for="currency in filteredCurrency" :currency="currency"></currency-detail>
+
+      <currency-detail v-if="noFilteredCurrency" v-for="currency in currencies" :currency="currency"></currency-detail>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import  CurrencyDetail from './currencyDetail.vue'
+
+export default {
+  name: 'currency-table',
+  props: ['currencies', 'filteredCurrency', 'selectedCurrency', 'noFilteredCurrency'],
+  components: {
+    'currency-detail': CurrencyDetail
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
