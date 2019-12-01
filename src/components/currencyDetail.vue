e6bdfdbc7169a10ee1172b73c8f920b9a80a7b36
L3n1ad <danieltoth0910@gmail.com>
2019-12-01 17:20:24 +0000

    diagram has been removed from detail page

--- src/components/currencyDetail.vue
+++ src/components/currencyDetail.vue
@@ -1,19 +1,20 @@
 <template lang="html">
   <tr>
-    <td><img :src="currency.image" style="max-width: 70px"></td>
+    <td><img :src="currency.image" style="max-width: 30px"></td>
     <td>{{currency.name}}</td>
     <td>{{currency.symbol}}</td>
-    <td>{{currency.current_price.toFixed(2)}}</td>
+    <td>GBP {{currency.current_price.toFixed(2)}}</td>
     <td>{{currency.price_change_percentage_24h_in_currency.toFixed(2)}}</td>
     <td>{{currency.price_change_percentage_7d_in_currency.toFixed(2)}}</td>
     <td>{{currency.price_change_percentage_30d_in_currency.toFixed(2)}}</td>
-    <currency-diagram :currency='currency'></currency-diagram>
+    <popup-window :currency='currency'></popup-window>
 
   </tr>
 </template>
 
 <script>
 import CurrencyDiagram from './currencyDiagram.vue'
+import  PopupWindow from './popupWindow.vue'
 
 export default {
   name: 'currency-detail',
@@ -24,7 +25,8 @@ export default {
     }
   },
   components: {
-    'currency-diagram': CurrencyDiagram
+    'currency-diagram': CurrencyDiagram,
+    'popup-window': PopupWindow
   }
 }
 </script>
