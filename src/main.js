7430f4277707e3a28f48cd1c901f7ebd2672065e
L3n1ad <danieltoth0910@gmail.com>
2019-12-01 17:20:01 +0000

    new npm imported globaly

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueGoogleCharts from 'vue-google-charts'
+import StackModal from '@innologica/vue-stackable-modal'
 
+Vue.use(StackModal);
 Vue.use(VueGoogleCharts);
 
 export const eventBus = new Vue();
